d8dbb0fcd6edb04dce498e33da9ab929ba21b01c
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-25 17:19:59 -0400

    API successfully deployed and working on heroku https://webapi-iv-challenge-dg.herokuapp.com/

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = require('./server');
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-    console.log('\n* Server running on http://localhost:5000 *\n')
+    console.log('\n* Server running on https://webapi-iv-challenge-dg.herokuapp.com/ *\n')
 });
