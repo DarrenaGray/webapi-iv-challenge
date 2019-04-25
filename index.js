3c06f6c8a4e13100251961f0e9c0377986ddb3c6
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-25 17:13:24 -0400

    Fixed syntax error on port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./server');
 
-const port = process.env.POST || 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
     console.log('\n* Server running on http://localhost:5000 *\n')
